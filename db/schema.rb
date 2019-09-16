13b1677bb373618332f58efd112163728822c774
David Hardy <davehardy632@gmail.com>
2019-09-16 10:08:01 -0600

    Creating migration and adding a sports table to the database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_160410) do
+ActiveRecord::Schema.define(version: 2019_09_16_160654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 2019_09_16_160410) do
     t.integer "weight"
     t.string "team"
     t.integer "total_medals_won", default: 0
+  end
+
+  create_table "sports", force: :cascade do |t|
+    t.string "sport"
   end
 
 end
