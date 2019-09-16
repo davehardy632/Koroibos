ee08b2b99b2672e9ddcd8c8a1e16632caa85ce9e
David Hardy <davehardy632@gmail.com>
2019-09-16 10:38:43 -0600

    Creating a model test for the olympian model.

---
+++ spec/models/olympian_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe Olympian do
+  describe "Validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :sex}
+    it {should validate_presence_of :age}
+    it {should validate_presence_of :height}
+    it {should validate_presence_of :weight}
+    it {should validate_presence_of :team}
+    it {should validate_presence_of :total_medals_won}
+  end
+
+  describe "Relationships" do
+    it {should have_many :olympian_events}
+    it {should have_many :events, through: :olympian_events}
+  end
+end
