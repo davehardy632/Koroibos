f5c03c7ed6fa413abb28e86c835d329545e4a8a2
David Hardy <davehardy632@gmail.com>
2019-09-16 10:43:31 -0600

    Creating a model test for the event model.

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe Event do
+  describe "Validations" do
+    it {should validate_presence_of :event}
+  end
+
+  describe "Relationships" do
+    it {should have_many :olympian_events}
+    it {should have_many :olympians, through: :olympian_events}
+  end
+end
