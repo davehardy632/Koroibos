1b6d992256560d6bca501bf86e05648b860da6a1
David Hardy <davehardy632@gmail.com>
2019-09-17 14:54:08 -0600

    Updating get all competitors test, showing records are returned alphabetically, aswell as returning the correct keys and format.

--- spec/requests/api/v1/get_all_competitors_spec.rb
+++ spec/requests/api/v1/get_all_competitors_spec.rb
@@ -4,7 +4,7 @@ describe "Returns a list of competitors for each sport" do
   before :each do
     @sport = Sport.create(sport: "Badminton")
     @olympian_1 = Olympian.create(name: "Jim Smith", sex: "M", age: 23, height: 122, weight: 234, team: "Canada", sport: "Badminton")
-    @olympian_1 = Olympian.create(name: "Bill Johnsob", sex: "M", age: 24, height: 133, weight: 264, team: "United States", sport: "Badminton")
+    @olympian_1 = Olympian.create(name: "Bill Johnson", sex: "M", age: 24, height: 133, weight: 264, team: "United States", sport: "Badminton")
     @olympian_1 = Olympian.create(name: "Kathy Bates", sex: "F", age: 28, height: 124, weight: 124, team: "Germany", sport: "Badminton")
   end
   it "When the sport id is send, all cometitor names are returned" do
@@ -14,7 +14,9 @@ describe "Returns a list of competitors for each sport" do
     expect(response).to be_successful
 
     competitors = JSON.parse(response.body)
-binding.pry
-    expect(competitors.count).to eq(3)
+
+    expect(competitors.keys).to eq(["sport", "competitors"])
+    # competitors are returned alphabetically
+    expect(competitors.values).to eq(["Badminton", ["Bill Johnson", "Jim Smith", "Kathy Bates"]])
   end
 end
