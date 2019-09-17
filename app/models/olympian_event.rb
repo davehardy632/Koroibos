2ecaa4a726038877a7931ce0ec91a428d1175bf0
David Hardy <davehardy632@gmail.com>
2019-09-17 13:48:31 -0600

    Creating a new model method to find olympians that earned a medal with the associated event.

--- app/models/olympian_event.rb
+++ app/models/olympian_event.rb
@@ -4,4 +4,8 @@ class OlympianEvent < ApplicationRecord
 
   validates_presence_of :medal
   validates_presence_of :games
+
+  def self.olympian_medalists(event_id)
+    OlympianEvent.where(event_id: event_id).where("olympian_events.medal != ?", "NA").joins(:olympian).select("olympian_events.medal as medal, olympians.id, olympians.name as name, olympians.team as team, olympians.age as age")
+  end
 end
