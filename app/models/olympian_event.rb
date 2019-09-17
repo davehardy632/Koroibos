e9feb0b3d69f93781b6c41ec9e6734ea19f08d24
David Hardy <davehardy632@gmail.com>
2019-09-17 16:44:43 -0600

    Changing medal values in the database to enums for comparison and sorting purposes.

--- app/models/olympian_event.rb
+++ app/models/olympian_event.rb
@@ -5,7 +5,9 @@ class OlympianEvent < ApplicationRecord
   validates_presence_of :medal
   validates_presence_of :games
 
+  enum medal: ["Gold", "Silver", "Bronze", "NA"]
+
   def self.olympian_medalists(event_id)
-    OlympianEvent.where(event_id: event_id).where("olympian_events.medal != ?", "NA").joins(:olympian).select("olympian_events.medal as medal, olympians.id, olympians.name as name, olympians.team as team, olympians.age as age")
+    OlympianEvent.where(event_id: event_id).where("olympian_events.medal != ?", 3).joins(:olympian).select("olympian_events.medal as medal, olympians.id, olympians.name as name, olympians.team as team, olympians.age as age").order("olympian_events.medal asc")
   end
 end
