29dbdef306fe92b022b9b1f8ae9e156ee566835e
David Hardy <davehardy632@gmail.com>
2019-09-17 13:42:28 -0600

    Creating a test to be comprehensive about the data returned, an event with associated medalists will return the olympians according to the project spec guidelines.

--- spec/requests/api/v1/get_all_medalists_spec.rb
+++ spec/requests/api/v1/get_all_medalists_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 describe "Return the medalists for an event" do
   before :each do
-    @sport_1 = Sport.create(sport: "Basketball")
-      @event_1 = @sport_1.events.create(event: "Men's Basketball")
-        @olympian_1 = @event_1.olympians.create(name: "James Adede", team: "United States", age: 24, medal: "Gold")
-        @olympian_2 = @event_1.olympians.create(name: "Evelina Afoa", team: "Egypt", age: 27, medal: "Bronze")
-        @olympian_3 = @event_1.olympians.create(name: "Macarena Aguilar Daz", team: "China", age: 22, medal: "Silver")
+    @sport_1 = Sport.create!(sport: "Basketball")
+      @event_1 = @sport_1.events.create!(event: "Men's Basketball")
+        @olympian_1 = Olympian.create!(name: "James Adede", sex: "M", age: 24, height: 123, weight: 156, team: "United States", total_medals_won: 1)
+          @oe_1 = OlympianEvent.create!(olympian_id: @olympian_1.id, event_id: @event_1.id, medal: "Gold", games: "2016 Summer")
+        @olympian_2 = Olympian.create!(name: "Evelina Afoa", sex: "F", age: 27, height: 112, weight: 122, team: "Egypt", total_medals_won: 1)
+          @oe_2 = OlympianEvent.create!(olympian_id: @olympian_2.id, event_id: @event_1.id, medal: "Silver", games: "2016 Summer")
+        @olympian_3 = Olympian.create!(name: "Macarena Aguilar Daz", sex: "F", age: 22, height: 122, weight: 144,  team: "China", total_medals_won: 1)
+          @oe_3 = OlympianEvent.create!(olympian_id: @olympian_3.id, event_id: @event_1.id, medal: "Bronze", games: "2016 Summer")
+        @olympian_4 = Olympian.create!(name: "Jim Smith", sex: "M", age: 25, height: 133, weight: 155,  team: "China", total_medals_won: 1)
+          @oe_4 = OlympianEvent.create!(olympian_id: @olympian_3.id, event_id: @event_1.id, medal: "NA", games: "2016 Summer")
   end
   it "Given the event id, return medalist name, team, age and medal" do
 
@@ -15,15 +20,15 @@ describe "Return the medalists for an event" do
     expect(response).to be_successful
 
     medalists = JSON.parse(response.body)
-binding.pry
+
     expect(medalists["medalists"].count).to eq(3)
     expect(medalists["medalists"][0].keys).to eq(["name", "team", "age", "medal"])
-    expect(medalists["medalists"][0].values).to eq(["James Adede", "United States", "24", "Gold"])
+    expect(medalists["medalists"][0].values).to eq(["James Adede", "United States", 24, "Gold"])
 
     expect(medalists["medalists"][1].keys).to eq(["name", "team", "age", "medal"])
-    expect(medalists["medalists"][1].values).to eq(["Macarena Aguilar Daz", "China", "22", "Silver"])
+    expect(medalists["medalists"][1].values).to eq(["Evelina Afoa", "Egypt", 27, "Silver"])
 
     expect(medalists["medalists"][2].keys).to eq(["name", "team", "age", "medal"])
-    expect(medalists["medalists"][2].values).to eq(["Evelina Afoa", "Egypt", "27", "Bronze"])
+    expect(medalists["medalists"][2].values).to eq(["Macarena Aguilar Daz", "China", 22, "Bronze"])
   end
 end
