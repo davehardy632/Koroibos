bfd0c7be4c9f450ad5ab89ddd11153e5dee10d7e
David Hardy <davehardy632@gmail.com>
2019-09-18 01:23:17 -0600

    Adding additional sad path testing to catch invalid country name, and invalid query parameters.

--- app/controllers/api/v1/medal_count_controller.rb
+++ app/controllers/api/v1/medal_count_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::MedalCountController < ApplicationController
 
   def index
-    render json: medal_count_facade.return_medal_data
+    if valid_country?
+      render json: medal_count_facade.return_medal_data
+    elsif !params.include?("country")
+      render json: missing_parameter_error
+    else
+      render json: error_message_response
+    end
   end
 
   private
@@ -10,7 +16,31 @@ class Api::V1::MedalCountController < ApplicationController
     params.permit("country")
   end
 
+  def valid_country?
+    if secure_facade.valid_country?
+      true
+    end
+  end
+
+  def secure_facade
+    SecureFacade.new(medal_count_params["country"])
+  end
+
   def medal_count_facade
     MedalCountFacade.new(medal_count_params["country"])
+  end
+
+  def error_message_response
+      {
+        error: "Country not Found",
+        status: 404
+      }
+  end
+
+  def missing_parameter_error
+      {
+        error: "Missing parameters",
+        status: 400
+      }
   end
 end
